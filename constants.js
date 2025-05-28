24bded4e52bb583d967b471d3babc5c12987b443
nayan-kunwar <nayankunwar678@gmail.com>
2025-05-28 15:43:21 +0530

    update cookie options for OAuth redirect handling in production

--- constants.js
+++ constants.js
@@ -2,8 +2,8 @@ export const DB_NAME = "Wander_Trip_DB";
 
 export const COOKIE_OPTIONS = {
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
-  secure: process.env.NODE_ENV !== "development",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // allow OAuth redirect
+  secure: process.env.NODE_ENV === "production",
 };
 
 export const USER_ROLES_ENUM = {
